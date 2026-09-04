847f5d793685b44b1b81b68b3d9a8f9a7b0b42f1
Maxime Dulieu <m.dulieu@outlook.fr>
2026-09-04 19:47:50 +0200

    minor changement

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -4,11 +4,9 @@ var is_ready: bool = true
 
 @onready
 var Level: Node2D = $".".get_parent().get_parent()
-#var ShootingPoint: Marker2D = $Sprite2D/ShootingPoint
 
 func _process(_delta: float) -> void:
 	pass
-
 
 func shoot(direction: Vector2) -> void:
 	is_ready = false
@@ -16,10 +14,8 @@ func shoot(direction: Vector2) -> void:
 	const PROJECTILE = preload("res://scenes/Projectile.tscn")
 	var new_projectile = PROJECTILE.instantiate()
 	new_projectile.global_position = global_position
-	new_projectile.global_rotation = global_rotation
 	new_projectile.direction = direction
 	Level.add_child(new_projectile)
-
 
 func _on_cooldown_timeout() -> void:
 	is_ready = true
