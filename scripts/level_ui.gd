29eefe2f5bb79d6dcf0c55c9afe64d5d0fb596a3
cugnett <cugnet.theophile@gmail.com>
2026-09-06 14:57:40 +0200

    Hearth working

--- scripts/level_ui.gd
+++ scripts/level_ui.gd
@@ -17,21 +17,26 @@ func _get_player_health() -> void:
 	player_health = get_parent().get_node("Player").health
 	
 func _display_player_health() -> void:
-	for i in range(1,player_health):
-		var inst = hearth.instantiate()
-		inst.position = life_position + i * Vector2(16,0) # inst.get_node("Sprite2D").texture.region.w
-		add_child.call_deferred(inst)
+	for i in range(0,player_health):
+		var hearth = hearth.instantiate()
+		hearth.position = life_position + i * Vector2(16,0) # inst.get_node("Sprite2D").texture.region.w
+		add_child.call_deferred(hearth)
 	
-func _update_display_player_health() -> void:
-	pass
-
+func _update_player_health(value) -> void:
+	var old_player_health = player_health
+	player_health += value
+	
+	var hearth_to_remove = old_player_health - player_health
+	print("Remove:" + str(hearth_to_remove))
+	print("for: " + str(old_player_health) + " to " + str(player_health))
+	if hearth_to_remove > 0:
+		for i in range(old_player_health, player_health, -1):
+			print("REMOVE!" + str(i))
+			if i > 0:
+				get_child(i-1).queue_free()
+	
 
 
 func _on_player_health_changed(value) -> void:
 	print("Health changed : " + str(value))
-	player_health += value
-	_update_display_player_health()
-
-
-func _on_player_player_is_in_room() -> void:
-	pass # Replace with function body.
+	_update_player_health(value)
