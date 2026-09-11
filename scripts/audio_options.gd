e66c3e53d465234adc9bc0d113eb85da67864001
Staphir <staphir@gmail.com>
2026-09-11 23:48:05 +0200

    sound option update in live + sfx noise when slide option sfx

--- scripts/audio_options.gd
+++ scripts/audio_options.gd
@@ -21,3 +21,19 @@ func _on_music_h_slider_mouse_exited() -> void:
 
 func _on_sfx_h_slider_mouse_exited() -> void:
 	release_focus()
+
+
+func _on_master_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db($VBoxContainer/MasterHSlider.value))
+
+
+func _on_music_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(1, linear_to_db($VBoxContainer/MusicHSlider.value))
+
+
+func _on_sfx_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(2, linear_to_db($VBoxContainer/SfxHSlider.value))
+
+
+func _on_sfx_h_slider_drag_ended(value_changed: bool) -> void:
+	$"../ButtonSound".play()
--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -12,9 +12,6 @@ func _process(_delta: float) -> void:
 
 
 func _on_confirm_button_pressed() -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db($AudioOptions/VBoxContainer/MasterHSlider.value))
-	AudioServer.set_bus_volume_db(1, linear_to_db($AudioOptions/VBoxContainer/MusicHSlider.value))
-	AudioServer.set_bus_volume_db(2, linear_to_db($AudioOptions/VBoxContainer/SfxHSlider.value))
 	SignalManager.validate_sound_options.emit()
 
 
