e51ab5af6da4a276e0e968e946c5f3519ffda8d7
cugnett <cugnet.theophile@gmail.com>
2026-09-13 18:18:17 +0200

    more enemies

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -75,14 +75,15 @@ func _activate_spawner(player_map_position: Vector2, player_position: Vector2) -
 	var min_enemy_number = 1
 	var max_enemy_number = room_ramp
 	var max_enemy_type = 0 #easy
-	if room_ramp > 0 and room_ramp <= 1:
+	if room_ramp == 1:
 		max_enemy_type = 0
-	if room_ramp >= 2 and room_ramp <= 3:
+	if room_ramp >= 2:
+		min_enemy_number = 2
 		max_enemy_type = 1 #medium
 	if room_ramp >= 4:
-		max_enemy_type = 2
-	if room_ramp >= 4:
-		min_enemy_number = 2
+		max_enemy_type = 2 #hard
+	if room_ramp >= 5:
+		min_enemy_number = 3
 		
 		
 	for i in randi_range(min_enemy_number,max_enemy_number):
